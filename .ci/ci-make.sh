7899594ad6717bbd057817b5a952a54e6593882a
Stefan Schmidt <s.schmidt@samsung.com>
2018-08-22 14:51:12 +0200

    WIP: test to build ewpi and see if docker image has been updated

--- .ci/ci-make.sh
+++ .ci/ci-make.sh
@@ -7,7 +7,10 @@ if [ "$1" = "release-ready" ] ; then
 fi
 travis_fold make make
 if [ "$DISTRO" != "" ] ; then
-  docker exec --env MAKEFLAGS="-j5 -rR" --env EIO_MONITOR_POLL=1 $(cat $HOME/cid) make
+  docker exec --env MAKEFLAGS="-j5 -rR" --env EIO_MONITOR_POLL=1 $(cat $HOME/cid) git clone https://github.com/vtorri/ewpi.git
+  docker exec --env MAKEFLAGS="-j5 -rR" --env EIO_MONITOR_POLL=1 $(cat $HOME/cid) cd ewpi
+  docker exec --env MAKEFLAGS="-j5 -rR" --env EIO_MONITOR_POLL=1 $(cat $HOME/cid) gcc -o ewpi ewpi.c ewpi_map.c $(pkg-config --cflags --libs libcurl)
+  docker exec --env MAKEFLAGS="-j5 -rR" --env EIO_MONITOR_POLL=1 $(cat $HOME/cid) ./ewpi $HOME/EFL/ewpi_64 x86_64-w64-mingw32
 else
   export PATH="$(brew --prefix gettext)/bin:$PATH"
   make
